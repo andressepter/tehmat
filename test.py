77127f5a3bb06990f34ae5652dadc40f5eb02d29
Student Side33 <student@taltech.ee>
2024-10-18 19:20:57 +0300

    poly

--- test.py
+++ test.py
@@ -3,3 +3,20 @@ import galois
 print('Hello, World!')
 GF = galois.GF(3**5)
 print(GF.properties)
+issubclass(GF, galois.FieldArray)
+issubclass(GF, np.ndarray)
+x = GF([236, 87, 38, 112]); x
+isinstance(x, galois.FieldArray)
+isinstance(x, np.ndarray)
+y = np.array([109, 17, 108, 224]); y
+y = y.view(GF); y
+GF.repr("poly"); x
+print (x)
+GF.repr("power"); x
+print (x)
+GF.repr("int");
+print (x)
+print (x + y)
+print (x - y)
+print (x * y)
+print (x / y)
