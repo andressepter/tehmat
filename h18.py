a8484f0680b200fe6b9094e78b517d268a369f35
Andres Septer <andres.septer@gmail.com>
2024-10-27 18:23:41 +0200

    h18

---
+++ h18.py
@@ -0,0 +1,17 @@
+import sys; sys.path.append('/home/asepter/.local/lib/python3.10/site-packages/')
+import galois as galois
+
+GF = galois.GF(7)
+
+# Ulesanne 1.8 Arvutage 5^2023 (mod 7) ja 14^2023 (mod 7)
+result = GF(5)**2023
+
+
+print(f"5^2023 (mod 7) järgi on {result}.")
+
+#y2 does not compute 14 > 7 
+# 14 = 2 x 7 
+
+# C === 2 mod 7 järgi
+# C === 7 mod 7 järgi
+#print (galois.crt([2,7],[7,7]))
