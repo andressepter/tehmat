8c5d38023ce7e0a3b400cd0f5141897aa031fec3
Andres Septer <andres.septer@gmail.com>
2024-10-27 18:23:47 +0200

    h18

--- h01.py
+++ h01.py
@@ -2,4 +2,5 @@
 import sys; sys.path.append('/home/asepter/.local/lib/python3.10/site-packages/')
 import galois as FF
 print (FF.egcd(34,24))
+#S ¨UT(34, 24) = A · 34 + B · 24
 #nb! süt on esimene väljund
