bb68c11c14a791c85ef4fd633ef76f48f637e111
Student Side33 <student@taltech.ee>
2024-11-04 11:50:43 +0200

    26?

---
+++ h26.py
@@ -0,0 +1,6 @@
+import sys; sys.path.append('/home/asepter/.local/lib/python3.10/site-packages/')
+import galois as galois
+import numpy as np
+
+korpus2x=galois.GF(2**9, repr="poly")
+print (list(galois.irreducible_polys(2,8)))
